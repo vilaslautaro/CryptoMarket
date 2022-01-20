d1ab9c7e59a6d9e64f76e649c5d6881925c89ff0
Laucha <vilaslautaro@gmail.com>
2022-01-19 21:14:40 -0300

    Le dimos estilos al boton btnMode que va a ejecutar o no el modo Nocturno

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,18 +3,21 @@ import './navBar.css';
 
 export default function NavBar() {
     return (
-        <div className='layout'>
+        <div id='layout' className='layout'>
             <header className='header'>
                 <div className='header__Nav'>
-                    <img className='nav__Logo' src='/logo.png'></img>
+                    <a href="#" className='nav_Enlace'><img className='nav__Logo' src='/logo.png'></img></a>
                     <ul className='nav__Menu'>
-                        <a href='#' className='menu__Enlaces'><li className='enlaces__Categorias'>Inicio</li></a>
-                        <a href='#' className='menu__Enlaces'><li className='enlaces__Categorias'>Productos</li></a>
-                        <a href='#' className='menu__Enlaces'><li className='enlaces__Categorias'>Servicios</li></a>
-                        <a href='#' className='menu__Enlaces'><li className='enlaces__Categorias'>Comprá Criptos</li></a>
-                        <a href='#' className='menu__Enlaces'><li className='enlaces__Categorias'>Contactanos</li></a>
+                        <a href='#' className='menu__Enlaces'><li className='menu__Categorias'>Inicio</li></a>
+                        <a href='#' className='menu__Enlaces'><li className='menu__Categorias'>Productos</li></a>
+                        <a href='#' className='menu__Enlaces'><li className='menu__Categorias'>Servicios</li></a>
+                        <a href='#' className='menu__Enlaces'><li className='menu__Categorias'>Comprá Criptos</li></a>
+                        <a href='#' className='menu__Enlaces'><li className='menu__Categorias'>Contactanos</li></a>
                     </ul>
-                    <button className='nav__BtnMode'>Claro/Oscuro</button>
+                    <button id="btnMode" className='nav__BtnMode'>
+                        <span><img className='btn__img Sun' src='/sun.png'></img></span>
+                        <span><img className='btn__img Moon' src='/moon.png'></img></span>
+                    </button>
                 </div>
                     <div className='header__SubNav'>
                         <form className='subNav__form'>
@@ -26,3 +29,18 @@ export default function NavBar() {
         </div>
     )
 };
+
+
+// funcion para el boton de modo Nocturno
+// const btnMode = document.getElementById('btnMode');
+// const divLayout = document.getElementById('layout');
+
+// export function modoNocturno(e){
+//     divLayout.classList.toogle('dark');
+//     btnMode.classList.toogle('activado');
+
+//     e.preventDefault();
+//     e.stopPropagation();
+// }
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// import mi primer component
 import NavBar from './components/navBar';
 
 ReactDOM.render(
@@ -12,7 +8,8 @@ ReactDOM.render(
     <NavBar />
   </React.StrictMode>,
   document.getElementById('root')
-);
+  );
+  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
